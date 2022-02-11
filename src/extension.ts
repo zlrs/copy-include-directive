819b2f579175a041447611eaf33f70cf037e4dab
Zhu Yuanqing <zhuys123@gmail.com>
2022-02-11 21:50:22 +0800

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,33 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as ncp from "copy-paste";
+
+function getProjectIncludeDirective(fileUri: vscode.Uri): string | null {
+	let relativePath = vscode.workspace.asRelativePath(fileUri);
+  let includeDirective: string = `#include "${relativePath}"`;
+  return includeDirective;
+}
+
+function copyIncludeDirective(selected: vscode.Uri): void {	
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+
+	let fileUri = editor.document.uri;
+	let includeDirective = getProjectIncludeDirective(fileUri);
+	if (includeDirective) {
+		ncp.copy<string>(includeDirective);
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	// current opened file in the opened editor
+	let disposable = vscode.commands.registerCommand(
+		'copy--include-directive.currentFile', copyIncludeDirective);
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
