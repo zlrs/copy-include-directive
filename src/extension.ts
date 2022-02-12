ef1dbc8b7bf9a4e3fb5c21e9b49ae87c93c8d462
Zhu Yuanqing <zhuys123@gmail.com>
2022-02-12 16:29:48 +0800

    add status bar item and preferences
    Fix grammer

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,21 @@ import * as vscode from 'vscode';
 import * as ncp from "copy-paste";
 
 function getProjectIncludeDirective(fileUri: vscode.Uri): string | null {
+class Config {
+	config: vscode.WorkspaceConfiguration;
+	constructor() {
+		this.config = vscode.workspace.getConfiguration('copyInclude');
+	}
+
+	get isShowStatusBar(): boolean {
+		return this.config.get('accessPoint.statusBar') as boolean;
+	}
+
+	get headerSearchPath(): Array<string> {
+		return [];
+	}
+}
+
 	let relativePath = vscode.workspace.asRelativePath(fileUri);
   let includeDirective: string = `#include "${relativePath}"`;
   return includeDirective;
@@ -31,17 +46,28 @@ function copyCurrentFileIncludeDirective(): void {
 	}
 }
 
-export function activate(context: vscode.ExtensionContext) {
-	// copy the #include directive of the current file in the editor
-	let disposable = vscode.commands.registerCommand(
-		'copy--include.currentFile', copyCurrentFileIncludeDirective);
-	
-	// copy the #include directive of the selected file in the explorer
-	let disposable2 = vscode.commands.registerCommand(
-		'copy--include.selectedFileInExplorer', copySelectedFileIncludeDirective);
+let statusBarItem: vscode.StatusBarItem;
 
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable2);
+export function activate(context: vscode.ExtensionContext) {
+	console.log('activated!');
+
+	const cmdIDCurrentFile = 'copy--include.currentFile';
+	const cmdIDSelectedFileInExplorer = 'copy--include.selectedFileInExplorer';
+	context.subscriptions.push(
+		// copy the #include directive of the current file in the editor
+		vscode.commands.registerCommand(cmdIDCurrentFile, copyCurrentFileIncludeDirective),
+		
+		// copy the #include directive of the selected file in the explorer
+		vscode.commands.registerCommand(cmdIDSelectedFileInExplorer, copySelectedFileIncludeDirective)
+	);
+
+	if (new Config().isShowStatusBar) {
+		statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+		statusBarItem.command = cmdIDCurrentFile;
+		statusBarItem.text = 'Copy #include';
+		statusBarItem.show();
+		context.subscriptions.push(statusBarItem);
 	}
+}
 
 export function deactivate() {}
